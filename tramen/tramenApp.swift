dba81a3ed787279b919a4a3a718be4d285b4a44e
SIMPLE MARK <57036815+THE-SIMPLE-MARK@users.noreply.github.com>
2026-03-01 02:50:04 +0100

    Initial Commit

---
+++ tramen/tramenApp.swift
@@ -0,0 +1,17 @@
+//
+//  tramenApp.swift
+//  tramen
+//
+//  Created by Márk Böszörményi on 2026.03.01.
+//
+
+import SwiftUI
+
+@main
+struct tramenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
